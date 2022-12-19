8edbb2a7e9d91c526606a088c6060838f11b57b6
kulotsystems <kulotsystems@gmail.com>
2022-12-19 14:16:04 +0800

    Lesson 2: Vue Components using TypeScript.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="app">
-        hello, hyrule
+        <p>Name: {{ name }}</p>
+        <p>Age: {{ age }}</p>
+        <button @click="changeName('Link')">Change Name</button>
+        <button @click="changeAge(29)">Change Age</button>
     </div>
 </template>
 
 
-<script setup lang="ts">
+<script lang="ts">
+    import { defineComponent } from 'vue';
 
+    export default defineComponent({
+        name: 'App',
+        components: {},
+        data() {
+            return {
+                name: 'Hyper',
+                age : 25 as number | string
+            }
+        },
+        methods: {
+            changeName(newName: string) {
+                this.name = newName;
+                return name;
+            },
+            changeAge(age: string | number) {
+                this.age = age;
+                return age;
+            }
+        }
+    });
 </script>
 
 
