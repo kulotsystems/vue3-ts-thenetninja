8f421a2ef55bcde957a841d050429e65b81cb65c
kulotsystems <kulotsystems@gmail.com>
2022-12-19 13:51:36 +0800

    Lesson 1: Intro & Setup.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
-<script setup lang="ts">
-    import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-    <div>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-        </a>
+    <div class="app">
+        hello, hyrule
     </div>
-    <HelloWorld msg="Vue + TS"/>
 </template>
 
+
+<script setup lang="ts">
+
+</script>
+
+
 <style scoped>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-    }
 
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    .logo.vue:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
-    }
 </style>
